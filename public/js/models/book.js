92d9b80c8bf4d17dab61e22d199d9cbf1593b53c
Sergey Sidorchick <whitekrokodile@gmail.com>
2013-11-04 23:47:37 +0300

    Application start logic added.

--- public/js/models/book.js
+++ public/js/models/book.js
@@ -3,7 +3,7 @@ var app = app || {};
 (function() {
 	app.Book = Backbone.Model.extend({
 		defaults: {
-			coverImage: 'img/placeholder',
+			coverImage: 'img/placeholder.png',
 			title: 'No title',
 			author: 'Unknown',
 			releaseDate: 'Unknown',
