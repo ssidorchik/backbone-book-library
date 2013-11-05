a91e1a580eefa01d4bf01823df8da2e4667fc3b1
Sergey Sidorchick <whitekrokodile@gmail.com>
2013-11-05 08:03:43 +0300

    Add/remove functionality implemented.

--- public/js/views/book.js
+++ public/js/views/book.js
@@ -4,10 +4,21 @@ var app = app || {};
 	app.BookView = Backbone.View.extend({
 		className: 'bookContainer',
 		template: _.template($('#bookTemplate').html()),
+		events: {
+			'click .delete': 'deleteBook'
+		},
+
+		initialize: function() {
+			this.listenTo(this.model, 'destroy', this.remove);
+		},
 
 		render: function() {
 			this.$el.html(this.template(this.model.toJSON()));
 			return this;
+		},
+
+		deleteBook: function() {
+			this.model.destroy();
 		}
 	});
 })();
--- public/js/views/library.js
+++ public/js/views/library.js
@@ -3,9 +3,15 @@ var app = app || {};
 (function() {
 	app.LibraryView = Backbone.View.extend({
 		el: '#books',
+		events: {
+			'click #add': 'addBook'
+		},
 
 		initialize: function(initialBooks) {
 			this.collection = new app.Library(initialBooks);
+
+			this.listenTo(this.collection, 'add', this.renderBook);
+
 			this.render();
 		},
 
@@ -18,6 +24,20 @@ var app = app || {};
 		renderBook: function(book) {
 			var bookView = new app.BookView({ model: book });
 			this.$el.append(bookView.render().el);
+		},
+
+		addBook: function(e) {
+			e.preventDefault();
+
+			var formData = {};
+
+			$('#addBook div').children('input').each(function(i, el) {
+				if ($(el).val() !== '') {
+					formData[el.id] = $(el).val();
+				}
+			});
+
+			this.collection.add(new app.Book(formData));
 		}
 	});
 })();
